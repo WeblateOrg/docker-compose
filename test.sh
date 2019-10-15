f301859b40bc13f30030917a061ebb816d1aaff7
Michal Čihař <michal@cihar.com>
2019-10-15 14:08:54 +0200

    Use docker-compose exec -T
    As per https://github.com/docker/compose/issues/5696
    
    Signed-off-by: Michal Čihař <michal@cihar.com>

--- test.sh
+++ test.sh
@@ -50,24 +50,24 @@ if [ $RET2 -ne 0 -o $RET -ne 0 ] ; then
 fi
 
 echo "Python packages:"
-docker-compose exec weblate pip3 list
+docker-compose exec -T weblate pip3 list
 
 echo "Deploy checks:"
-docker-compose exec \
+docker-compose exec -T \
     --user weblate \
     --env WEBLATE_SILENCED_SYSTEM_CHECKS=weblate.E003,weblate.E017,security.W004,security.W008,security.W012,security.W018,weblate.I021 \
     weblate weblate check --deploy || exit 1
 
 echo "Creating admin..."
-docker-compose exec --user weblate weblate weblate createadmin --update || exit 1
+docker-compose exec -T --user weblate weblate weblate createadmin --update || exit 1
 
 echo "Supervisor status:"
-docker-compose exec weblate supervisorctl status all || exit 1
+docker-compose exec -T weblate supervisorctl status all || exit 1
 
 if [ $TESTS = yes ] ; then
 echo "Running testsuite..."
-docker-compose exec --user weblate --env DJANGO_SETTINGS_MODULE=weblate.settings_test weblate weblate collectstatic --noinput
-docker-compose exec --user weblate --env DJANGO_SETTINGS_MODULE=weblate.settings_test weblate weblate test --noinput weblate.accounts weblate.trans weblate.lang weblate.api weblate.gitexport weblate.screenshots weblate.utils weblate.machinery weblate.auth weblate.formats weblate.fonts weblate.addons weblate.wladmin weblate.vcs
+docker-compose exec -T --user weblate --env DJANGO_SETTINGS_MODULE=weblate.settings_test weblate weblate collectstatic --noinput
+docker-compose exec -T --user weblate --env DJANGO_SETTINGS_MODULE=weblate.settings_test weblate weblate test --noinput weblate.accounts weblate.trans weblate.lang weblate.api weblate.gitexport weblate.screenshots weblate.utils weblate.machinery weblate.auth weblate.formats weblate.fonts weblate.addons weblate.wladmin weblate.vcs
 if [ $? -ne 0 ] ; then
     docker-compose logs
     exit 1
